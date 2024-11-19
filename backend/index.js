6f6effa3b91e80ef468a706339021a6477fee4b6
DivyaGupta04 <itsme.divya.0405@gmail.com>
2024-11-20 00:25:29 +0530

    dockerfiles added

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ var mysql = require("mysql2");
 var http = require("http");
 var url = require("url");
 http.createServer(function(req,res){
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080"); 
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); 
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.writeHead(200,{"Content-Type":"application/json"});
@@ -116,9 +116,10 @@ http.createServer(function(req,res){
         }
     })
 }).listen(8080);
+
 var con = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"@123Diya@",
+    password:"@123diya@",
     database:"node"
 })
