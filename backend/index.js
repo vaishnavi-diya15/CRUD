42974863b56868430a6f82243b9dd52695a323de
vaishnavi-diya15 <vaishnavi.diya15@gmail.com>
2024-11-20 01:32:38 +0530

    small changes in "index.js"

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ var mysql = require("mysql2");
 var http = require("http");
 var url = require("url");
 http.createServer(function(req,res){
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080"); 
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); 
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); 
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.writeHead(200, { "Content-Type": "application/json" });
